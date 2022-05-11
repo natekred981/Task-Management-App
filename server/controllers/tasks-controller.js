fb07cf64e24774f3a171cc24a98ae50dcf062ef3
Nathan Kredentser <nathan@Nathans-MacBook-Air.local>
2022-05-11 12:49:03 -0400

    created post request for mongoose and backend

--- server/controllers/tasks-controller.js
+++ server/controllers/tasks-controller.js
@@ -6,29 +6,6 @@ import PostMessage from "../models/postMessage.js";
 //createPost is designed to save a post that we create by getting the body of the 
 //request we made, and then saving that body to somewhere
 
-// export const getPosts = async (req, res) => {
-//     try {
-//         const postMessage = await PostMessage.find();
-//         res.status(200).json(postMessage);
-//     }
-//     catch (error) {
-//         res.status(404).json({message: error.message});
-//     }
-// }
-
-// export const createPost = async (req, res) => {
-//     const post = req.body;
-//     const newPost = new PostMessage(post);
-    
-//     try {
-        
-//         await newPost.save();
-//         res.status(201).json(newPost);
-//     }
-//     catch (error){
-//         res.status(409).json({message: error.message});
-//     }
-// }
 
 const Dummy_form = [{
     id: uuid.v4(),
@@ -49,17 +26,30 @@ export const getTasksByUser = (req, res, next) => {
     res.json({tasks});
 };
 
-export const postNewTask = (req, res, next) => {
+export const postNewTask = async (req, res, next) => {
     const {title, option, description, creator} = req.body;
-    const createdTask = {
+    const createdTask = new PostMessage({
         title,
         option,
         description,
         creator
-    };
+    });
 
-    Dummy_form.push(createdTask);
-    res.status(201).json(createdTask);
+    await createdTask.save(createdTask);
+    res.status(201).json({message: createdTask});
+
+
+    // const createdProduct = new PostMessage({
+    //     title: req.body.title,
+    //     option: req.body.option,
+    //     description: req.body.description,
+    //     creator: req.body.creator
+    // });
+
+    // createdProduct._id.toString();
+    // const result = await createdProduct.save();
+    // console.log(createdProduct);
+    // res.json(result);
 
 };
 
